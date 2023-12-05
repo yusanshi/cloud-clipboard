4b7e7ae4c3cc3cd4b53f5631c6ac27fbcd9520b2
yusanshi <meet.leiyu@gmail.com>
2023-12-06 00:01:09 +0800

    Finish

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import fire
 import re
 import logging
-import pyautogui
+import requests
 
 from pathlib import Path
 from time import sleep
@@ -58,34 +58,76 @@ def get_clipboard():
             'xclip -selection clipboard -target text/plain -out')
         return 'text', text
     else:
-        return None
+        return None, None
 
 
-def copy():
+def copy(host, prefix, username, password):
     logging.info('Copy')
     window_name = run_get_output('xdotool getactivewindow getwindowname')
     logging.info(f'Current active window name: {window_name}')
     if any([re.match(pattern, window_name) for pattern in CTRL_SHIFT_C_REGEX]):
         logging.info('Copy with Ctrl+Shift+C')
-        # pyautogui.hotkey('ctrl', 'shift', 'c')
         run_silent('xdotool key ctrl+shift+c')
     else:
         logging.info('Copy with Ctrl+C')
-        # pyautogui.hotkey('ctrl', 'c')
         run_silent('xdotool key ctrl+c')
     sleep(0.2)
 
-    data = get_clipboard()
-    if data is None:
+    type, data = get_clipboard()
+    if type is None:
         logging.info('Clipboard empty')
+        return
+
+    logging.info(
+        f"Clipboard data:\n\ttype: {type}\n\tlength: {len(data)}\n\tpreview: {repr(data[:64])}..."
+    )
+
+    session = requests.Session()
+    session.auth = (username, password)
+    if type == 'image':
+        session.put(f"{host}SET/{prefix}:clipboard:image", data=data)
+        session.get(f"{host}DEL/{prefix}:clipboard:text")
+    elif type == 'text':
+        session.put(f"{host}SET/{prefix}:clipboard:text",
+                    data=data.encode('utf-8'))
+        session.get(f"{host}DEL/{prefix}:clipboard:image")
+
+    logging.info('Upload successfully')
+
+
+def paste(host, prefix, username, password):
+    logging.info('Paste')
+    session = requests.Session()
+    session.auth = (username, password)
+
+    text = session.get(
+        f"{host}GET/{prefix}:clipboard:text.txt").content.decode('utf-8')
+    image = session.get(f"{host}GET/{prefix}:clipboard:image.png").content
+    if text and not image:
+        subprocess.run('xclip -selection clipboard -target text/plain -in',
+                       shell=True,
+                       input=text,
+                       text=True)
+        logging.info('Write text to clipboard')
+    elif not text and image:
+        subprocess.run('xclip -selection clipboard -target image/png -in',
+                       shell=True,
+                       input=image)
+        logging.info('Write image to clipboard')
     else:
         logging.info(
-            f"Clipboard data:\n\ttype: {data[0]}\n\tlength: {len(data[1])}\n\tpreview: {repr(data[1][:64])}{'...' if len(data[1])>64 else ''}"
+            f'Error data from cloud:\ntext: {repr(text[:64])}...\nimage: {repr(image[:64])}...'
         )
 
-
-def paste():
-    logging.info('Paste')
+    sleep(0.2)
+    window_name = run_get_output('xdotool getactivewindow getwindowname')
+    logging.info(f'Current active window name: {window_name}')
+    if any([re.match(pattern, window_name) for pattern in CTRL_SHIFT_C_REGEX]):
+        logging.info('Paste with Ctrl+Shift+V')
+        run_silent('xdotool key ctrl+shift+v')
+    else:
+        logging.info('Paste with Ctrl+V')
+        run_silent('xdotool key ctrl+v')
 
 
 if __name__ == '__main__':
